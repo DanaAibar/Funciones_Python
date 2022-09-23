ee804af724c1395177b6bf045182e6bbe44c6b06
Dana Aibar <dana.aibar@mi.unc.ar>
2022-09-22 21:27:43 -0300

    Segunda_copia_proyecto.

---
+++ funciones.py
@@ -0,0 +1,10 @@
+print("Hola Mundo")
+
+#Funciones predefinidas --> del lenguaje
+#Funciones propias --> las que creamos los desarrolladores
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo isntrucciones basicas")
+print("Poco a poco iremos avanzando.")
+
+#AREA PARA MIS FUNCIONES
