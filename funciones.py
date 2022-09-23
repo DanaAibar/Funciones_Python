d170178b959e87cdacb11999ec6cc358709c3dbf
Dana Aibar <dana.aibar@mi.unc.ar>
2022-09-22 21:36:26 -0300

    Tercera_parte_proyecto.

--- funciones.py
+++ funciones.py
@@ -1,10 +1,22 @@
+#AREA PARA MIS FUNCIONES --> Reutilizacion de codigo
+def mensaje(): #Declaracion
+    print("Estamos aprendiendo Python")
+    print("Estamos aprendiendo isntrucciones basicas")
+    print("Poco a poco iremos avanzando.")
+
+
 print("Hola Mundo")
 
 #Funciones predefinidas --> del lenguaje
 #Funciones propias --> las que creamos los desarrolladores
 
-print("Estamos aprendiendo Python")
-print("Estamos aprendiendo isntrucciones basicas")
-print("Poco a poco iremos avanzando.")
+# Llamo a mis funciones, las veces que necesito.
+mensaje() #Llamada funcion
 
-#AREA PARA MIS FUNCIONES
+mensaje()
+
+mensaje()
+
+
+
+
